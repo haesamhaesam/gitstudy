e97485e07c9d5ea7056989ad1c822b825991e73a
ohchaeyul\olikk <olikkaan9108@gmail.com>
2026-03-23 17:04:54 +0900

    로컬 브랜치 생성 및 커밋

--- Source/gitstudy/gitstudy.h
+++ Source/gitstudy/gitstudy.h
@@ -1,6 +1,25 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// MyActor.cpp
 
-#pragma once
 
-#include "CoreMinimal.h"
+#include "MyActor.h"
+
+AMyActor::AMyActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+}
+
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+}
 
